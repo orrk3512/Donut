7c4f3a9981489d0f288f518643a8ed0c780d09d3
Kaleb Orr <rpg3512@gmail.com>
2026-03-07 16:58:27 -0700

    first variation of the donut mathmatical model

--- donut.cpp
+++ donut.cpp
@@ -40,8 +40,8 @@ public:
         }
     }
 
-    // Runs function f on all points within the matrix with the input being the coordinattes of that point
-    void modify(float (*f)(int, int, int))
+    // Runs function f on all points within the matrix with the input being the coordinattes of that point and the resolution
+    void modify(float (*f)(int, int, int, int))
     {
         for (int i = 0; i < m.size(); ++i)
         {
@@ -49,7 +49,7 @@ public:
             {
                 for (int k = 0; k < m.at(i).at(j).size(); k++)
                 {
-                    m.at(i).at(j).at(k) = f(i, j, k);
+                    m.at(i).at(j).at(k) = f(i, j, k, resolution);
                 }
             }
         }
@@ -61,7 +61,7 @@ private:
 };
 
 // Determines whether a point in a 3d matrix is inside the mathmatical representation of a donut
-float donutToMatrix();
+float donutToMatrix(int x, int y, int z, int resolution);
 
 void render(const std::vector<std::vector<std::vector<float>>> &matrix);
 
@@ -69,12 +69,31 @@ float translate(float in);
 
 void main()
 {
-    matrix m(1);
-    m.modify(translate);
+    matrix m(32);
+    m.modify(donutToMatrix);
 }
 
-float donutToMatrix() {
+float donutToMatrix(int x, int y, int z, int resolution) {
+    // Offset from the origin of the matrix to the center of the of the cartesian space
+    const float offset = 0.5 * resolution;
+    // Radius from the center of mass to outer edge of the donut
+    const float outer_radius = 0.7 * resolution;
+    // Radius from the inside of the tube to outside of the tube of the torus
+    const float tube_radius = 0.2 * resolution;
 
+    // transformations to cartesian space
+    const float x_cart = x - offset;
+    const float y_cart = y - offset;
+    const float z_cart = z - offset;
+
+    // Distance from the point to the torus formed by the tube_radius
+    // Equation of torus is: (outer_radius - sqrt(x^2 + y^2))^2 + z^2 <= tube_radius^2
+    const float torus = std::sqrt(std::pow(outer_radius - std::sqrt(std::pow(x_cart, 2) + std::pow(y_cart, 2)), 2) + std::pow(z_cart, 2));
+    if (torus <= tube_radius) {
+        return 1.0;
+    } else {
+        return 0.0;
+    }
 }
 
 float translate(float in)
