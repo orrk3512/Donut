03131081f65d4205ec13bc7ea911cad2b56847f7
Kaleb Orr <rpg3512@gmail.com>
2026-03-07 15:26:57 -0700

    switched to a more class oriented structure

--- donut.cpp
+++ donut.cpp
@@ -3,9 +3,45 @@
 #include <cmath>
 #include <assert.h>
 
-std::vector<std::vector<std::vector<float>>> create3D(int resolution);
+class matrix
+{
+public:
+    matrix(int resolution)
+    {
+        m.assign(resolution, {});
+        for (int i = 0; i < m.size(); ++i)
+        {
+            m.at(i).assign(resolution, {});
+            for (int j = 0; j < m.at(i).size(); ++j)
+            {
+                m.at(i).at(j).assign(resolution, {});
+                for (int k = 0; k < m.at(i).at(j).size(); k++)
+                {
+                    m.at(i).at(j).at(k) = 0;
+                }
+            }
+        }
+    }
 
-void modify(std::vector<std::vector<std::vector<float>>> &matrix, float (*f)(float));
+    void modify(float (*f)(float))
+    {
+        for (int i = 0; i < m.size(); ++i)
+        {
+            for (int j = 0; j < m.at(i).size(); ++j)
+            {
+                for (int k = 0; k < m.at(i).at(j).size(); k++)
+                {
+                    m.at(i).at(j).at(k) = f(m.at(i).at(j).at(k));
+                }
+            }
+        }
+    }
+
+private:
+    std::vector<std::vector<std::vector<float>>> m;
+};
+
+float donutToMatrix(float in);
 
 void render(const std::vector<std::vector<std::vector<float>>> &matrix);
 
@@ -13,41 +49,11 @@ float translate(float in);
 
 void main()
 {
-    std::vector<std::vector<std::vector<float>>> m;
-    m = create3D(1);
-    m.at(0).at(0).at(0) = 1.0;
-
-    modify(m, translate);
+    matrix m(1);
+    m.modify(translate);
 }
 
-std::vector<std::vector<std::vector<float>>> create3D(int resolution) {
-    assert(resolution > 0);
-
-    std::vector<std::vector<std::vector<float>>> m;
-    m.assign(resolution, {});
-    for (int i = 0; i < m.size(); ++i) {
-        m.at(i).assign(resolution, {});
-        for (int j = 0; j < m.at(i).size(); ++j) {
-            m.at(i).at(j).assign(resolution, {});
-            for (int k = 0; k < m.at(i).at(j).size(); k++) {
-                m.at(i).at(j).at(k) = 0;
-            }
-        }
-    }
-    return m;
-}
-
-void modify(std::vector<std::vector<std::vector<float>>> &matrix, float (*f)(float))
+float translate(float in)
 {
-    for (int i = 0; i < matrix.size(); ++i) {
-        for (int j = 0; j < matrix.at(i).size(); ++j) {
-            for (int k = 0; k < matrix.at(i).at(j).size(); k++) {
-                matrix.at(i).at(j).at(k) = f(matrix.at(i).at(j).at(k));
-            }
-        }
-    }
-}
-
-float translate(float in) {
-    return 0;
+    return 1;
 }
